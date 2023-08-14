7255e7d7b29b7c024997c83c836598ba19dbe92f
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2023-08-15 00:29:02 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import Navbar from "@/componenten/navbar";
+//import Home from "@/scenes/home";
+//import OurClasses from "@/scenes/ourClasses";
+//import Benefits from "@/scenes/benefits";
+//import ContactUs from "@/scenes/contactUs";
+//import Footer from "@/scenes/footer";
 import { useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
-// type Props = {
-//   isTopOfPage: boolean;
-//   selectedPage: SelectedPage;
-//   setSelectedPage: (value: SelectedPage) => void;
-// };
 
 const App = () => {
-   const [selectedPage, setSelectedPage] = useState<string>("home");
-
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  
 
  return (
     <div className="app bg-gray-20 ">
        <Navbar
          selectedPage={selectedPage}
-         setSelectedPage={setSelectedPage}/>
-       
+       setSelectedPage={setSelectedPage} />
+      {/* <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
+        */}
     </div>
    
  );
